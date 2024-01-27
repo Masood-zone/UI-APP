3f88a0c52df3ce275ab9a4b3160ce813ebd672e5
Masood Acheampong <masoodacheampong@gmail.com>
2024-01-27 22:43:48 +0000

    Made changes to the id generator and the colors

--- index.js
+++ index.js
@@ -1,18 +1,25 @@
-const input = document.getElementById('input');
-const list = document.getElementById('list');
+const input = document.getElementById("input");
+const list = document.getElementById("list");
 const todoList = [];
-let value;
+let userValue;
 
 function saveInput(event) {
-  value = event.target.value
-  console.log(value)
+  userValue = event.target.value;
+  console.log(userValue);
 }
 
 function addTask() {
-  const todo = { id: todoList.length = 0 ? 1 : (todoList.length - 1) + 1, value: value }
-  todoList.push(todo)
+  const todo = {
+    id: Math.floor(Math.random() * 100),
+    value: userValue,
+  };
+  todoList.push(todo);
   list.innerHTML = todoList.map(function (todo) {
-    return '<li key={todo.id} class="flex items-center justify-between bg-white rounded-md border-2 border-gray-300 p-1">' + todo.value + '</li>';
-  })
+    return (
+      '<li key={todo.id} class="flex items-center justify-between bg-white rounded-md border-2 border-gray-300 py-1 px-1">' +
+      todo.value +
+      "</li>"
+    );
+  });
   input.value = "";
 }
