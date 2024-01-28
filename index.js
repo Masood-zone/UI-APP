58886cf1cb0726753a5d2c54b00e997d1d910cea
Faiza <issahfaiza777@gmail.com>
2024-01-27 20:09:42 -0800

    Implemented usiing HTML and JavaScript in Todo app

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const input = document.getElementById('input');
+const list = document.getElementById('list');
+const todoList = [];
+let value;
+
+function saveInput(event) {
+  value = event.target.value
+  console.log(value)
+}
+
 function addTask() {
-    const taskInput = document.getElementById('taskInput');
-    const taskList = document.getElementById('taskList');
-    
-    if (taskInput.value !== '') {
-      const li = document.createElement('li');
-      li.textContent = taskInput.value;
-      taskList.appendChild(li);
-      taskInput.value = '';
-    }
-  }
+  const todo = { id: todoList.length = 0 ? 1 : (todoList.length - 1) + 1, value: value }
+  todoList.push(todo)
+  list.innerHTML = todoList.map(function (todo) {
+    return '<li key={todo.id} class="flex items-center justify-between bg-white rounded-md border-2 border-gray-300 p-1">' + todo.value + '</li>';
+  })
+  input.value = "";
+}
